9e4fe7fbceb062f6f40f3a599a6e0e0140dd181b
rajeshdg <43397133+rajeshdg@users.noreply.github.com>
2023-03-31 09:09:53 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World ***Rajesh†****!";
+        return "Hello World ***Rajesh****!";
     }
 
     public static void main(String[] args) {
